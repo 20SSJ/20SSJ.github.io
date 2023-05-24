da805e357a2c81a39a1a08b09376bcf41424186c
20SSJ <64567484+ssjn01@users.noreply.github.com>
2023-05-24 11:52:32 +0900

    자바스크립트 분리하기

---
+++ size.js
@@ -0,0 +1,8 @@
+function zoomIn(event){
+    event.target.style.transform="scale(1.5)";
+    event.target.style.transition="all 0.5s";
+}
+function zoomOut(event){
+    event.target.style.transform="scale(1)";
+    event.target.style.transition="all 0.5s";
+}
