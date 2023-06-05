41c2202d4a6517453e838b055000a48dee68215b
20SSJ <64567484+ssjn01@users.noreply.github.com>
2023-06-05 17:48:51 +0900

    파비콘 넣기

--- size.js
+++ size.js
@@ -1,5 +1,5 @@
 function zoomIn(event){
-    event.target.style.transform="scale(1.1)";
+    event.target.style.transform="scale(1.05)";
     event.target.style.transition="all 0.5s";
 }
 function zoomOut(event){
